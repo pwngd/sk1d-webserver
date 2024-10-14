3244aef07de4accff0dc088c461fa92d2437826d
whopper9999 <testwhopper9000@gmail.com>
2024-10-14 23:16:06 +0000

    fixes

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const path = require('path');
 const server = fastify({ logger: true });
 
 server.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
+  root: path.join(__dirname, 'public')
 });
 
 const start = async () => {
   try {
-    await server.listen(80);
+    await server.listen({ port: 80 });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
-};
+}
 
 start();
