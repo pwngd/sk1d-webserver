82e745b0dad91df70708db69c33228a8873700a9
Debian <admin@ip-172-31-25-201>
2024-10-15 17:14:53 +0000

    add stuff

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.register(fastifyStatic, {
 
 const start = async () => {
   try {
-    await server.listen({ port: 80 });
+    await server.listen({ port: 3000 });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
