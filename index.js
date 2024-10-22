3525d07b07a0996faca5b17877d5a63c5c30faf1
whopper9999 <whopper9000@gmail.com>
2024-10-22 18:21:28 +0000

    EJS implementation

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const fastify = require("fastify");
-const fastifyStatic = require("@fastify/static");
 const path = require("path");
+const fastifyStatic = require("@fastify/static");
+const pov = require("@fastify/view");
+const ejs = require("ejs");
 
 const server = fastify({ logger: true });
 
 server.register(fastifyStatic, {
   root: path.join(__dirname, "public")
+});
+
+server.register(pov, {
+  engine: { ejs },
+  root: path.join(__dirname, "views"),
+  layout:"./layouts/layout.ejs"
+});
+
+server.get("/", async (req, rep) => {
+  return rep.viewAsync("index",  { title:"homepage" });
+});
+
+server.get("/test", async (req, rep) => {
+  return rep.viewAsync("test",  { title:"testpage" });
 });
 
 const start = async () => {
